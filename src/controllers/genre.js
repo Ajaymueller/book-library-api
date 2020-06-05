a4590715da077ea8f203a96fb8f410b69069c235
Aidan Mueller <Aidan_Mueller@hotmail.co.uk>
2020-06-05 18:27:41 +0100

    helper function used for genre controller

--- src/controllers/genre.js
+++ src/controllers/genre.js
@@ -7,37 +7,12 @@ const {
   deleteItem,
 } = require('./helpers');
 
-/*exports.createGenre = async (req, res) => {
-  try {
-    const genre = await Genre.create(req.body);
-    res.status(201).json(genre);
-  } catch (error) {
-    const errorMessages = await error.errors.map((e) => e.message);
-    res.status(400).json({ errors: errorMessages });
-  }
-};*/
-
 exports.createGenre = async (req, res) => createItem(res, 'genre', req.body);
 
-exports.listGenres = async (req, res) => {
-  const genres = await Genre.findAll();
-  res.status(200).json(genres);
-};
+exports.listGenres = async (req, res) => getAllItems(res, 'genre');
 
-exports.getGenreById = async (req, res) => {
-  const { genreId } = req.params;
-  const genre = await Genre.findByPk(genreId);
-  !genre
-    ? res.status(404).json({ error: 'The genre could not be found.' })
-    : res.status(200).json(genre);
-};
+exports.getGenreById = async (req, res) =>
+  getItemById(res, 'genre', req.params.genreId);
 
-exports.deleteGenreById = async (req, res) => {
-  const { genreId } = req.params;
-  const genre = await Genre.findByPk(genreId);
-  !genre
-    ? res.status(404).json({ error: 'The genre could not be found.' })
-    : Genre.destroy({ where: { id: genreId } }).then(() =>
-        res.status(204).send()
-      );
-};
+exports.deleteGenreById = async (req, res) =>
+  deleteItem(res, 'genre', req.params.genreId);
